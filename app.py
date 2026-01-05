cbb174ea1b717a0d407a17606340254b95a5384d
kaaviyaa27072003 <kaaviyaa27072003@gmail.com>
2026-01-05 12:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import streamlit as st
 
 model_path = Path(__file__).parent / "model.pkl"
 
-with open(model_path, "rb") as f:
-    model = pickle.load(f)
+if st.button("Predict"):
+    st.success("This is a demo — no real model is loaded 🙂")
+
 
 st.title("ML Model Deployment with Streamlit 🎯")
 st.write("Enter input values below:")
@@ -19,6 +20,7 @@ if st.button("Predict"):
     input_data = np.array([[feature1, feature2, feature3]])
     prediction = model.predict(input_data)
     st.success(f"Prediction: {prediction[0]}")
+
 
 
 
